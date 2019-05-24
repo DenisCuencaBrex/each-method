78db8c02d666dd0d0483644914adb6eabba43d6b
Denis Cuenca Brex <Deniscbrex.dev@gmail.com>
2019-05-24 12:37:35 -0300

    changes in the methods

--- my_each.rb
+++ my_each.rb
@@ -1,46 +1,58 @@
 module Enumerable
     def my_each
-       for i in self
-        yield(i)
-       end 
+       for i in 0..self.length-1
+        yield(self[i]) if block_given?
+       end
+       self
     end
 
     def my_each_with_index
-        i = 0
-        arr = []
-        loop do
-            arr << yield(self[i], i)
-            i += 1
-            break if i > self.length - 1
-        end
-        arr
+       for i in 0..self.length-1
+        yield(self[i], i) if block_given?
+       end
+       self
     end
 
     def my_select
-        arr = []
-        self.my_each{|x| arr << x if yield(x)}
-        arr 
+        if block_given?
+            arr = []
+            self.my_each{|x| array_new << x if yield(self)}
+            arr
+        end
     end
 
     def my_all?
-        self.my_each{|x| return false unless yield(x)}
-        true
+        if block_given?
+            self.my_each{|x| return false if yield(x) == false}
+            true
+        else
+            true
+        end
     end
 
     def my_any?
-        
+        if block_given?
+            self.my_each{|x| return true if yield(x) == true}
+            false
+        else
+            true
+        end
     end
 
     def my_none?
-
+        if block_given?
+            self.my_each{|x| return false if yield(x) == true}
+            true
+        else
+            false
     end
 
     def my_count
         
     end
 
-    def my_map
-
+    def my_map(&block)
+    
     end
 
     def my_inject
