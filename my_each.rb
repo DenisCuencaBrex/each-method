98dff328b9c7896017f4d4a6900d959c5d0bf447
Denis Cuenca Brex <Deniscbrex.dev@gmail.com>
2019-05-24 12:14:18 -0300

    added my_each, my_each_with_index, my_select and my_all methods

--- my_each.rb
+++ my_each.rb
@@ -1,6 +1,51 @@
 module Enumerable
     def my_each
+       for i in self
+        yield(i)
+       end 
+    end
+
+    def my_each_with_index
+        i = 0
+        arr = []
+        loop do
+            arr << yield(self[i], i)
+            i += 1
+            break if i > self.length - 1
+        end
+        arr
+    end
+
+    def my_select
+        arr = []
+        self.my_each{|x| arr << x if yield(x)}
+        arr 
+    end
+
+    def my_all?
+        self.my_each{|x| return false unless yield(x)}
+        true
+    end
+
+    def my_any?
         
     end
+
+    def my_none?
+
+    end
+
+    def my_count
+        
+    end
+
+    def my_map
+
+    end
+
+    def my_inject
+
+    end
+
     
 end
