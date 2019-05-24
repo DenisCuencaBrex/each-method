2ec325985fcbab3244d73097aa0634769018cfe7
Denis Cuenca Brex <Deniscbrex.dev@gmail.com>
2019-05-24 14:23:08 -0300

    added all methods and tests

--- my_each.rb
+++ my_each.rb
@@ -16,7 +16,7 @@ module Enumerable
     def my_select
         if block_given?
             arr = []
-            self.my_each{|x| array_new << x if yield(self)}
+            self.my_each{|x| arr << x if yield(self)}
             arr
         end
     end
@@ -45,19 +45,92 @@ module Enumerable
             true
         else
             false
+        end
     end
 
     def my_count
-        
+        if block_given?
+            number = 0
+            self.my_each{|x| number += 1 if yield(x)}
+            number
+        else
+            puts "not block given"
+        end
     end
+end
 
-    def my_map(&block)
-    
+
+    def my_map(&proc)
+        if block_given?
+            arr = []
+            self.my_each{|x| arr << yield(x)}
+            arr
+        else
+            puts "not block given"
+        end
     end
 
     def my_inject
-
+        if nil
+            total = nil
+        else
+            total = self[0]
+    end
+    for i in 1..self.length
+        total = yield(total, i)
+    end
+    total
     end
 
-    
 end
+
+def multiply_els(array)
+    array.my_inject{|total, number| total * number}
+end
+
+#TEST
+
+#my_each
+[3, 4, 10, 2, 6, 9].my_each {|x| p x }
+
+#my_each_with_index
+hash = {
+  "Hey" => 0,
+  "hello" => 1,
+  "Hi" => 2,
+  "bye" => 3
+}
+
+hash.my_each_with_index{|x, index|hash[x] = index}
+
+puts hash
+
+#my_select
+arr = [1,3,4,5,6,7]
+arr.my_select{ |num| num.even?}  
+
+#my_all?
+arr1 = ["hi", "Hello", "Hey", "goodbye"]
+puts arr1.my_all?{|item| item.length > 1}
+
+#my_any?
+puts arr1.my_any?{|item| item.length == 2}
+
+#my_none
+puts arr1.my_none?{|item| item.length < 10}
+
+#my_count
+arr2 = [1, 3, 4, 8, 10]
+puts arr2.my_count{ |x| x%4==0 }  
+
+#my_map
+puts arr2.my_map{|x| x*x}
+
+#my_inject
+arr3 = [2,4,4]
+puts arr3.my_inject{|sum, n| sum + n}
+puts arr3.my_inject{|mul, n| mul * n}
+
+#multiply_els
+p mulptiply_els([2, 4, 5])  
+
