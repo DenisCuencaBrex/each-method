b3515f8a0d527cee9d25cda16c2b514a24af7efb
Denis Cuenca Brex <Deniscbrex.dev@gmail.com>
2019-06-08 17:39:35 -0300

    added all unit testing

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,7 @@
 module Enumerable
     def my_each
        for i in 0..self.length-1
-        yield(self[i]) if block_given?
+        yield(self[i]) if self.is_a? Array
        end
        self
     end
@@ -43,7 +43,7 @@ module Enumerable
             self.my_each{|x| return true if yield(x) == true}
             false
         else
-            false
+            true
         end
     end
 
@@ -63,8 +63,6 @@ module Enumerable
         number
     end
 
-
-
     def my_map(&proc)
         if block_given?
             arr = []
@@ -75,15 +73,27 @@ module Enumerable
         end
     end
 
-    def my_inject
-     arr = self.to_a
-        until arr.length == 1
-            arr[0] = yield(arr[0], arr[1])
-            arr.delete_at(1)
+    def my_inject(acc="undefined")
+     arr = self
+        if self.is_a? Array
+        if acc == "undefined"
+          acc = self[0]
+          arr = self[1..-1]
         end
-        arr[0]
+        arr.my_each do |element|
+          acc = yield(acc, element)
+        end
+      elsif self.is_a? Hash
+        if acc == "undefined"
+          acc = self[self.keys[0]]
+          arr.delete(arr.keys[0])
+        end
+        arr.my_each do |key, element|
+          acc = yield(acc, key, element)
+        end
+      end
+      acc
     end
-
 end
 
 def multiply_els(array)
--- spec/my_each_spec.rb
+++ spec/my_each_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe Enumerable do
         end
 
         it "Works with hash" do
-            expect(hash.my_each{|x, val| x}).to eql(hash)
+            expect(hash.my_each{|key, val| key}).to eql(hash)
         end
 
         it "Works with proc" do
@@ -54,11 +54,15 @@ RSpec.describe Enumerable do
         end
         
         it "Returns true if the elements in the hash pass any condition, if it's not returns false" do
-            expect(hash1.my_all?{|key, val| val == 1}).to eql(false)
+            expect(hash1.my_all?{|key, val| val.length > 1}).to eql(true)
         end
 
         it "Returns false if find nil value in array" do
             expect(array_nil.my_all?{|val| val == nil}).to eql(false)
+        end
+
+        it "Works with procs" do
+            expect(array.my_all?{block}).to eql(array.my_all?{block})
         end
     end
 
@@ -68,36 +72,73 @@ RSpec.describe Enumerable do
         end
 
         it "Returns true if it find the value in the hash" do
-            expect(hash.my_any? {|key, value| value == 5}).to eql(false)
+            expect(hash1.my_any? {|key, val| val.length < 1}).to eql(false)
         end
 
         it "Returns false if not block passed" do
             expect(array.my_any?{}).to eql(false)
         end
 
+        it "Works with procs" do
+            expect(array.my_any?{block}).to eql(array.my_any?{block})
+        end
+
     end
 
     describe "#my_none?" do
         it "Returns true if the elements in the array pass any condition, if it's not returns false" do
-            expect(["Hi", "Hello", "Hey", "Goodbye"].my_none? {|item| item.length < 10}).to eql(false)
+            expect(array.my_none? {|item| item < 10}).to eql(false)
         end
+
+        it "Returns true if the elements in the hash pass any condition, if it's not returns false" do
+            expect(hash1.my_none? {|key, val| val.length > 2}).to eql(true)
+        end
+
+        it "Returns true if not block passed" do
+            expect(array.my_none? {}).to eql(true)
+        end
+
+        it "Works with procs" do
+            expect(array.my_none?{block}).to eql(array.my_none?{block})
+        end
+
     end
 
     describe "#my_count" do
         it "Returns the numbers of element in the array" do
-            expect([1,2,3,4,5,6].my_count).to eql(6)
+            expect(array.my_count).to eql(4)
         end
+
+        it "Returns the numbers of element in the hash" do
+            expect(hash.my_count).to eql(2)
+        end
+
     end
 
-        describe "#my_map" do
-        it "Returns a new array containing the values of multiply all the elements in the array for itself returned by the block" do
-            expect([1,2,3,4].my_map {|x| x * x}).to eql([1,4,9,16])
+    describe "#my_map" do
+        it "Returns a new array containing the values of multiply all the elements in the array for itself" do
+            expect(array.my_map {|x| x * x}).to eql([1,4,9,16])
         end
     end
 
     describe "#my_inject" do
-        it "Returns all the elements in the array into one element"do
-            expect([2,2,4].my_inject {|a, b| a + b}).to eql(8)
+        it "Returns all the elements in the array into one element" do
+            expect(array.my_inject {|a, b| a + b}).to eql(10)
+        end
+
+        it "Works with hashes" do
+            expect(hash.my_inject{|acc, key, val| acc + val}).to eql(1)
+        end
+
+        it "Works with procs" do
+            expect(array.my_inject{block}).to eql(array.my_inject{block})
+        end
+
+    end
+
+    describe "#multiply_els(array)" do
+        it "multiplies the elements in the array" do
+            expect(multiply_els(array)).to eql(24)
         end
     end
 
